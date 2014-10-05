f0a6eae00ae6e9782b2d61793ffdb3c444a5a7bc
Andrej Zachar <andrej@chocolatejar.eu>
2014-10-05 14:04:13 -0700

    Added a brand new home page based on the bootstrap 'Agency' theme Using PrismJS (Okaida) Provided better examples

---
+++ js/picker-examples.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var module = angular.module('exampleApp', ['language-picker','ngSanitize']);
+  module.controller('MainCtrl', function ($scope, $modal,$compile) {
+    /*$scope.codeSamples = [
+    'var module = angular.module("myApp", ["language-picker"]);',
+    '<language-picker languages="[\'en-US\', \'fr-FR\']" on-change="someCallback(language)" flags="true" template="custom.tpl.html">\n   Choose language\n</language-picker>',
+    '<language-picker languages="[\'en-US\', \'fr-FR\']" on-change="someCallback(language)">Choose language</language-picker>'
+    ];*/
+
+   $scope.codeSamples = {
+     basic: '<language-picker languages="[\'en-US\',\'de\',\'fr\']" on-change="onLanguageChange(language)">\n  Language Picker\n</language-picker>',
+     typical: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)">\n  Language Picker\n</language-picker>',
+     noFlags: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)"  flags="false">\n  Language Picker\n</language-picker>',
+     customModal: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)"  flags="false">\n  Language Picker\n</language-picker>',
+     attributes: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)" template="custom.tpl.html">\n   Choose language\n</language-picker>',
+     supportedLangs: ['en-US', 'es-AR', 'sk-SK', 'zh-CN', 'fa-IR', 'fr', 'he-IL', 'ru', 'de-CH']
+   };
+});
+
+module.controller('SampleCtrl', function ($scope, $modal) {
+
+  $scope.show = false;
+  $scope.onLanguageChange = function (language) {
+    $scope.language = language;
+    $scope.show = true;
+  };
+});
