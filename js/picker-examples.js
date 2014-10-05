12ce141743f0b3f9fe44df476e7e1ee9e98da7f1
Andrej Zachar <andrej@chocolatejar.eu>
2014-10-05 14:19:58 -0700

    Fixed languages displayed in examples

--- js/picker-examples.js
+++ js/picker-examples.js
@@ -7,13 +7,14 @@ var module = angular.module('exampleApp', ['language-picker','ngSanitize']);
     '<language-picker languages="[\'en-US\', \'fr-FR\']" on-change="someCallback(language)" flags="true" template="custom.tpl.html">\n   Choose language\n</language-picker>',
     '<language-picker languages="[\'en-US\', \'fr-FR\']" on-change="someCallback(language)">Choose language</language-picker>'
     ];*/
+    var lgs = '[\'en-US\', \'es-AR\', \'sk-SK\', \'zh-CN\', \'fa-IR\', \'fr\', \'he-IL\', \'ru\', \'de-CH\']';
 
    $scope.codeSamples = {
      basic: '<language-picker languages="[\'en-US\',\'de\',\'fr\']" on-change="onLanguageChange(language)">\n  Language Picker\n</language-picker>',
-     typical: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)">\n  Language Picker\n</language-picker>',
-     noFlags: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)"  flags="false">\n  Language Picker\n</language-picker>',
-     customModal: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)"  flags="false">\n  Language Picker\n</language-picker>',
-     attributes: '<language-picker languages="codeSamples.supportedLangs" on-change="onLanguageChange(language)" template="custom.tpl.html">\n   Choose language\n</language-picker>',
+     typical: '<language-picker languages="'+lgs+'" on-change="onLanguageChange(language)">\n  Language Picker\n</language-picker>',
+     noFlags: '<language-picker languages="'+lgs+'" on-change="onLanguageChange(language)"  flags="false">\n  Language Picker\n</language-picker>',
+     customModal: '<language-picker languages="'+lgs+'" on-change="onLanguageChange(language)"  flags="false">\n  Language Picker\n</language-picker>',
+     attributes: '<language-picker languages="'+lgs+'" on-change="onLanguageChange(language)" template="custom.tpl.html">\n   Choose language\n</language-picker>',
      supportedLangs: ['en-US', 'es-AR', 'sk-SK', 'zh-CN', 'fa-IR', 'fr', 'he-IL', 'ru', 'de-CH']
    };
 });
